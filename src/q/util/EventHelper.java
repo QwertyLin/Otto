63289dc534220a211d672c19c8a1291122250e06
Administrator <Administrator@172.18.3.36>
2013-01-11 18:25:53 +0800

    Q

--- src/q/util/EventHelper.java
+++ src/q/util/EventHelper.java
@@ -2,6 +2,12 @@ package q.util;
 
 import com.squareup.otto.Bus;
 
+/**
+ * @author Administrator
+ * 
+ * 监听器的应用可以减少模块间代码的耦合度, 使代码结构更加清晰. 实现所谓的"职责分离"
+ *
+ */
 public final class EventHelper {
 
 	private static Bus BUS;
